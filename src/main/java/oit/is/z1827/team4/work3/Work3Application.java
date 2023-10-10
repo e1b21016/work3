9d50f26736109b011f6bfca4d00795f453096f0a
e1b21016 <e1b21016@oit.ac.jp>
2023-10-10 14:55:54 +0900

    リポジトリの初期化する

---
+++ src/main/java/oit/is/z1827/team4/work3/Work3Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1827.team4.work3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work3Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1827/team4/work3/Work3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1827.team4.work3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
